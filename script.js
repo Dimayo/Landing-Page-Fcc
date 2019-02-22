7fd767b5e6ca5c654c0200bdd4d1e032aa813b5a
Dimayo <44707838+Dimayo@users.noreply.github.com>
2019-02-23 03:52:16 +0600

    Menu toggler changed

--- script.js
+++ script.js
@@ -7,6 +7,8 @@
         if (navLinks.hasOwnProperty(n)) {
             navLinks[n].addEventListener('click', (e) => {
                 e.preventDefault();
+                document.querySelector('.nav-links').classList.remove('show');
+                document.getElementById('burger-menu').checked = false;
                 document.querySelector(navLinks[n].hash).scrollIntoView({
                     block: 'start',
                     behavior: 'smooth'
@@ -14,4 +16,38 @@
             });
         }
     }
+})();
+
+//Toggle Menu
+
+(function menuToggle() {
+    "use strict";
+    document.querySelector('.burger-menu').addEventListener('click', () => {
+        const nav = document.querySelector('.nav-links');
+        if (!nav.classList.contains('show')) {
+            nav.classList.add('show');
+        } else {
+            nav.classList.remove('show');
+        }
+    });
+})();
+
+//Scroll change
+
+(function scrollChange() {
+    "use strict";
+    const parts = document.querySelectorAll('.part');
+
+    window.onscroll = () => {
+        let scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+        scrollPos += window.innerHeight / 2;
+
+        for (let s in parts) {
+            if (parts.hasOwnProperty(s) && parts[s].offsetTop <= scrollPos) {
+                const id = parts[s].id;
+                document.querySelector('.active').classList.remove('active');
+                document.querySelector(`a[href*=${id}]`).classList.add('active');
+            }
+        }
+    };
 })();
